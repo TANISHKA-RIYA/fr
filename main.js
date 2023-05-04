9a4b71debbc9ac1ccd7a03d827edfca457c84764
TANISHKA-RIYA <106598143+TANISHKA-RIYA@users.noreply.github.com>
2023-05-04 20:19:19 +0530

    p105

--- main.js
+++ main.js
@@ -20,4 +20,25 @@ console.log("ml5 version",ml5.version);
 
 classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/_BiAbRpwa/model.json",modelLoaded)
 
+function modelLoaded(){
+    console.log("Model is loaded!!");
+}
+
+function check(){
+img=document.getElementById("image");
+classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+    if (error){
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("object").innerHTML=results[0].label;
+        document.getElementById("accuracy").innerHTML=(results[0].confidence*100).toFixed(2)+" %";
+    }
+
+}
+
 //https://teachablemachine.withgoogle.com/models/_BiAbRpwa/
